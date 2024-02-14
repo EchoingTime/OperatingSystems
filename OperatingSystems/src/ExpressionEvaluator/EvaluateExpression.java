a39c71f7cb6287a7116eb6cb6bd6561acd7aeae0
dante_anz <dante_anz@localhost>
2024-02-13 21:50:39 -0500

    Changed Ordering

--- OperatingSystems/src/ExpressionEvaluator/EvaluateExpression.java
+++ OperatingSystems/src/ExpressionEvaluator/EvaluateExpression.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 import java.util.Stack;
 /**
  * EvaluateExpression - A Class that takes an expression given by the user and with 
- * accordance to order of precedence, prints out the operations formatted like a tree data 
- * structure but using stacks. The program will also print out the correct solution of the 
- * expression. This program is for simple operations only, such as multiplication, division, addition, 
- * and subtraction, expressions provided by the user must be in InFlix form ((e.g. 5 + 2 / 10 + 3), and
- * the expression must contain single-digit values (e.g. no greater than 9)
+ * accordance to order of precedence, prints out the operation step-by-step. The program will also print 
+ * out the correct solution of the expression. This program is for simple operations only, such as 
+ * multiplication, division, addition, and subtraction, expressions provided by the user must be in InFlix 
+ * form ((e.g. 5 + 2 / 10 + 3), and the expression must contain single-digit values (e.g. no greater than 9)
  * @author Dante Anzalone
  * @version 2023-09 (4.29.0)
  * @references 
@@ -50,95 +49,35 @@ public class EvaluateExpression
 		System.out.printf("%nAnswer: %s%n"
 				+ "%n=============================================================================%n", result);
 		
-		printInTreeForm();
+		print();
 	}
 	
 	/**
-	 * evaluation Method - Will get the PostFix Conversion and evaluate the expression
-	 * @return result - The evaluated expression
-	 * @referenced - PseudoCode via https://www.youtube.com/watch?v=UKuIw8cKKsc&ab_channel=EZCSE
-	 */
-	public double evaluation ()
-	{	
-		double tempResult, tempValue, valueOne, valueTwo;
-		
-		tempValue = 0; valueOne = 0; valueTwo = 0; tempResult = 0;
-		
-		for (int i = 0; i < postfix.size(); i++)
-		{
-			// Not an operator but an operand...
-			if (!(postfix.get(i).equals("*")) && !(postfix.get(i).equals("/")) && !(postfix.get(i).equals("+")) && !(postfix.get(i).equals("-")))
-			{
-				tempValue = Double.parseDouble(postfix.get(i)); // Parsing into a double
-				values.push(tempValue); // Pushed onto the stack
-			}
-			else if ((postfix.get(i).equals("*")) || (postfix.get(i).equals("/")) || (postfix.get(i).equals("+")) || (postfix.get(i).equals("-")))
-			{
-				valueTwo = values.pop(); // Pop "second" value from stack
-				valueOne = values.pop(); // Pop "first" value from stack
-				tempResult = performOperation(valueOne, valueTwo, postfix.get(i)); 
-				values.push(tempResult); // Pushes it back onto the stack
-			}
-		}
-		return values.pop(); // The last value
-	}
-	
-	/**
-	 * performOperation Method - Will perform the operation as determined in the evaulation's method
-	 * via incoming parameters. Will return the result back to the public method.
-	 * @param valueOne - First value
-	 * @param valueTwo - Second value
-	 * @param operator - Operator Type
-	 * @return result - The result of the operation
-	 */
-	private double performOperation (double valueOne, double valueTwo, String operator)
-	{
-		double result;
-		result = 0;
-				
-		if (operator.equals("+"))
-		{
-			result = valueOne + valueTwo;
-		}
-		else if (operator.equals("-"))
-		{
-			result = valueOne - valueTwo;
-		}
-		else if (operator.equals("*"))
-		{
-			result = valueOne * valueTwo;
-		}
-		else if (operator.equals("/"))
-		{
-			result = valueOne / valueTwo;
-		}
-		else
-		{
-			System.out.printf("%nError in performing operation - No valid operator%n");
-			System.exit(0);
-		}
-		
-		equationsWithAnswer.add(valueOne + " " + operator + " " + valueTwo);
-		equationsWithAnswer.add("  " + result);
-
-		return result;
-	}
-	
-	/**
-	 * printInTreeForm Method - Prints the user equation and the step-by-step process in tree form 
+	 * userPrompt Method - Asks the user for an equation to solve
 	 * @param None
-	 * @return None
+	 * @return equation - A string given via user and formatted to include no white
+	 * spaces
 	 */
-	private void printInTreeForm ()
+	public String userPrompt ()
 	{
-		System.out.printf("Output of Expressions"
-				+ "%n%s", unseperated);
+		String equation;
+		Scanner scan;
 		
-		for (int i = 0; i < equationsWithAnswer.size(); i++)
-		{
-				System.out.printf("%n%s", equationsWithAnswer.get(i));
-		}
-		System.out.printf("%n=============================================================================%n");
+		scan = new Scanner (System.in);
+		System.out.printf("%nEvaluationExpression Program by Dante Anzalone%n%n=============================================================================%n"
+				+ "%nBefore entering your expression, make sure to follow the Program's Rules:%n"
+				+ "%n1. Simple operations only (*, /, +, and -)"
+				+ "%n2. Must be in Inflix Expression form (e.g. 5 + 2 / 10 + 3)"
+				+ "%n3. Deals with single digit numbers (e.g. Don't do any value no greater than 9)%n"
+				+ "%n=============================================================================%n");
+		
+		System.out.printf("%nInput an equation to solve: ");
+		equation = scan.nextLine();
+		unseperated = equation;
+		equation = equation.replace(" ", ""); // Ensures there is no white space
+		
+		scan.close();
+		return equation;
 	}
 	
 	/**
@@ -217,30 +156,90 @@ public class EvaluateExpression
 	}
 	
 	/**
-	 * userPrompt Method - Asks the user for an equation to solve
-	 * @param None
-	 * @return equation - A string given via user and formatted to include no white
-	 * spaces
+	 * evaluation Method - Will get the PostFix Conversion and evaluate the expression
+	 * @return result - The evaluated expression
+	 * @referenced - PseudoCode via https://www.youtube.com/watch?v=UKuIw8cKKsc&ab_channel=EZCSE
 	 */
-	public String userPrompt ()
+	public double evaluation ()
+	{	
+		double tempResult, tempValue, valueOne, valueTwo;
+		
+		tempValue = 0; valueOne = 0; valueTwo = 0; tempResult = 0;
+		
+		for (int i = 0; i < postfix.size(); i++)
+		{
+			// Not an operator but an operand...
+			if (!(postfix.get(i).equals("*")) && !(postfix.get(i).equals("/")) && !(postfix.get(i).equals("+")) && !(postfix.get(i).equals("-")))
+			{
+				tempValue = Double.parseDouble(postfix.get(i)); // Parsing into a double
+				values.push(tempValue); // Pushed onto the stack
+			}
+			else if ((postfix.get(i).equals("*")) || (postfix.get(i).equals("/")) || (postfix.get(i).equals("+")) || (postfix.get(i).equals("-")))
+			{
+				valueTwo = values.pop(); // Pop "second" value from stack
+				valueOne = values.pop(); // Pop "first" value from stack
+				tempResult = performOperation(valueOne, valueTwo, postfix.get(i)); 
+				values.push(tempResult); // Pushes it back onto the stack
+			}
+		}
+		return values.pop(); // The last value
+	}
+	
+	/**
+	 * performOperation Method - Will perform the operation as determined in the evaulation's method
+	 * via incoming parameters. Will return the result back to the public method.
+	 * @param valueOne - First value
+	 * @param valueTwo - Second value
+	 * @param operator - Operator Type
+	 * @return result - The result of the operation
+	 */
+	private double performOperation (double valueOne, double valueTwo, String operator)
 	{
-		String equation;
-		Scanner scan;
+		double result;
+		result = 0;
+				
+		if (operator.equals("+"))
+		{
+			result = valueOne + valueTwo;
+		}
+		else if (operator.equals("-"))
+		{
+			result = valueOne - valueTwo;
+		}
+		else if (operator.equals("*"))
+		{
+			result = valueOne * valueTwo;
+		}
+		else if (operator.equals("/"))
+		{
+			result = valueOne / valueTwo;
+		}
+		else
+		{
+			System.out.printf("%nError in performing operation - No valid operator%n");
+			System.exit(0);
+		}
 		
-		scan = new Scanner (System.in);
-		System.out.printf("%nEvaluationExpression Program by Dante Anzalone%n%n=============================================================================%n"
-				+ "%nBefore entering your expression, make sure to follow the Program's Rules:%n"
-				+ "%n1. Simple operations only (*, /, +, and -)"
-				+ "%n2. Must be in Inflix Expression form (e.g. 5 + 2 / 10 + 3)"
-				+ "%n3. Deals with single digit numbers (e.g. Don't do any value no greater than 9)%n"
-				+ "%n=============================================================================%n");
+		equationsWithAnswer.add(valueOne + " " + operator + " " + valueTwo);
+		equationsWithAnswer.add("  " + result);
+
+		return result;
+	}
+	
+	/**
+	 * printInTreeForm Method - Prints the user equation and the step-by-step process 
+	 * @param None
+	 * @return None
+	 */
+	private void print ()
+	{
+		System.out.printf("Output of Expressions"
+				+ "%n%s", unseperated);
 		
-		System.out.printf("%nInput an equation to solve: ");
-		equation = scan.nextLine();
-		unseperated = equation;
-		equation = equation.replace(" ", ""); // Ensures there is no white space
-		
-		scan.close();
-		return equation;
+		for (int i = 0; i < equationsWithAnswer.size(); i++)
+		{
+				System.out.printf("%n%s", equationsWithAnswer.get(i));
+		}
+		System.out.printf("%n=============================================================================%n");
 	}
 }
